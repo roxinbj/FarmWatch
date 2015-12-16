98882855efb360020c297d9b46e1fcfdb8f48956
Björn Roxin <roxinbj@h20x.fritz.box>
2015-12-16 15:54:46 +0100

    Initial Commit

---
+++ FarmWatch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FarmWatch
+//
+//  Created by Björn Roxin on 12/16/15.
+//  Copyright © 2015 Björn Roxin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
